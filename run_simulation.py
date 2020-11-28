d7e99359833a7c49009e733eb67f8e8f94ab00cc
Glen Wasserfall <glen@wasserfalls.co.za>
2020-11-29 00:19:42 +0200

    added colour to scenario output

--- run_simulation.py
+++ run_simulation.py
@@ -4,19 +4,24 @@ from simulation.scenarios import *
 if __name__ == "__main__":
     # boot_environment()
  
-    print("Simulating many buy requests")
+    print("\u001b[36m=== Simulating many buy requests ===\u001b[0m")
+    
     buy_instrument_many_times("Crypto", "ETH", 10)
 
-    print("\n\nSimulating many sell requests")
+    print("\n\n\u001b[36m=== Simulating many sell requests ===\u001b[0m")
+    
     sell_instrument_many_times("Crypto", "ETH", 10)
 
-    print("\n\nSimulating unknown market")
+    print("\n\n\u001b[36m=== Simulating unknown market ===\u001b[0m")
+    
     order_unknown_market("SomeCrazyNameThatDoesntExist")
 
-    print("\n\nSimulating unknown instrument")
-    order_unknown_instrument("WhatIsThisThatIWantToBuy")
+    print("\n\n\u001b[36m=== Simulating unknown instrument ===\u001b[0m")
 
-    print("\n\nSimulating bad amounts")
+    order_unknown_instrument("WhatIsThisThatIWantToBuy ===")
+
+    print("\n\n\u001b[36m=== Simulating bad amounts ==\u001b[0m")
+    
     order_bad_amounts()
 
 
